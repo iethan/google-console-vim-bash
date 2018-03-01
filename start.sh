3068ba3758c751c8c55129637b2bda2981317227
Ethan <ethanl@seerinteractive.com>
2018-03-01 15:18:36 -0500

    initial commit

---
+++ start.sh
@@ -0,0 +1,2 @@
+sudo curl  https://raw.githubusercontent.com/sickill/vim-monokai/master/colors/monokai.vim --output /usr/share/vim/vim80/colors/monokai.vim
+sudo printf "\ncolorscheme monokai\nset number\nset viminfo='20,<1000,s1000" >> /usr/share/vim/vimrc
